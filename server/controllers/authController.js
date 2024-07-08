4dd0f3726c06dc92f18364d9f295d203a1c4f3a9
Anuj Kumar <99342265+AnujIstwal@users.noreply.github.com>
2024-07-08 15:31:07 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,25 +26,27 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).send("User not found");
-        }
-        const isMatch = await user.comparePassword(password);
-        if (!isMatch) {
-            return res.status(400).send("Invalid credentials");
-        }
-        // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        //     expiresIn: "1h",
-        // });
-        const token = jwt.sign(
-            { userId: user._id, userEmail: user.email },
-            process.env.JWT_SECRET
-        );
-        res.status(200).json({ token });
-    } catch (error) {
-        res.status(400).send("Error logging in: " + error.message);
-    }
+    // const { email, password } = req.body;
+    // try {
+    //     const user = await User.findOne({ email });
+    //     if (!user) {
+    //         return res.status(400).send("User not found");
+    //     }
+    //     const isMatch = await user.comparePassword(password);
+    //     if (!isMatch) {
+    //         return res.status(400).send("Invalid credentials");
+    //     }
+    //     // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    //     //     expiresIn: "1h",
+    //     // });
+    //     const token = jwt.sign(
+    //         { userId: user._id, userEmail: user.email },
+    //         process.env.JWT_SECRET
+    //     );
+    //     res.status(200).json({ token });
+    // } catch (error) {
+    //     res.status(400).send("Error logging in: " + error.message);
+    // }
+
+    res.json("Login working");
 };
