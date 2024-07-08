767918e6e9da540f119c2370f158b794f31d4323
Anuj Kumar <99342265+AnujIstwal@users.noreply.github.com>
2024-07-08 15:57:56 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,14 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 exports.userDetails = async (req, res) => {
-    const userEmail = req.user.userEmail;
+    // const userEmail = req.user.userEmail;
 
-    try {
-        const user = await User.findOne({ email: userEmail });
-        res.json(user);
-    } catch (error) {
-        res.json(`Error : ${error.message}`);
-    }
+    // try {
+    //     const user = await User.findOne({ email: userEmail });
+    //     res.json(user);
+    // } catch (error) {
+    //     res.json(`Error : ${error.message}`);
+    // }
+    res.json("api/auth working");
 };
 
 exports.register = async (req, res) => {
@@ -26,27 +27,27 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    // const { email, password } = req.body;
-    // try {
-    //     const user = await User.findOne({ email });
-    //     if (!user) {
-    //         return res.status(400).send("User not found");
-    //     }
-    //     const isMatch = await user.comparePassword(password);
-    //     if (!isMatch) {
-    //         return res.status(400).send("Invalid credentials");
-    //     }
-    //     // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    //     //     expiresIn: "1h",
-    //     // });
-    //     const token = jwt.sign(
-    //         { userId: user._id, userEmail: user.email },
-    //         process.env.JWT_SECRET
-    //     );
-    //     res.status(200).json({ token });
-    // } catch (error) {
-    //     res.status(400).send("Error logging in: " + error.message);
-    // }
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send("User not found");
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid credentials");
+        }
+        // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        //     expiresIn: "1h",
+        // });
+        const token = jwt.sign(
+            { userId: user._id, userEmail: user.email },
+            process.env.JWT_SECRET
+        );
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(400).send("Error logging in: " + error.message);
+    }
 
-    res.json("Login working");
+
 };
